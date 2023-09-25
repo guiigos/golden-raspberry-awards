ed5045696ee7505a72f988e9a820eca0885f7e7e
Guilherme Alves <guiigos.alves@gmail.com>
2023-09-25 00:06:34 -0300

    feat: Configure envs, tailwind and mapping of requests

---
+++ src/@types/domain.d.ts
@@ -0,0 +1,49 @@
+declare namespace Domain {
+  interface Year {
+    year: number;
+    winnerCount: number;
+  }
+
+  interface Years {
+    years: Year[];
+  }
+
+  interface Studio {
+    name: string;
+    winCount: number;
+  }
+
+  interface Studios {
+    studios: Studio[];
+  }
+
+  interface MinMax {
+    producer: string;
+    interval: number;
+    previousWin: number;
+    followingWin: number;
+  }
+
+  interface Interval {
+    min: MinMax[];
+    max: MinMax[];
+  }
+
+  interface Movie {
+    id: number;
+    year: number;
+    title: string;
+    winner: boolean;
+  }
+
+  interface List {
+    content: Movie[];
+    first: boolean;
+    last: boolean;
+    number: number;
+    numberOfElements: number;
+    size: number;
+    totalElements: number;
+    totalPages: number;
+  }
+}
